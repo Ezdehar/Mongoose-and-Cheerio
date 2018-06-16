8d1bed1bc1d66b13246f9c25dd6d1dc693bbeb77
Ezdehar Jaber <ezdeharjaber@Ezdehars-MacBook-Pro.local>
2018-06-16 11:19:31 -0400

    update

--- server.js
+++ server.js
@@ -1,101 +1,105 @@
 var express = require("express");
 var cheerio = require("cheerio");
-var $ = cheerio.load('<ul id="">...</ul>');
-var mongojs = require("mongojs");
 var request = require("request");
 var mongoose = require("mongoose");
-var express = require("express");
 var exphbs  = require('express-handlebars');
 var bodyParser = require("body-parser");
 
 var app = express();
 
-request('http://www.nytimes.com', function (error, response, body) {
-  console.log('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-});
+// Require our routes
+var routes = require("./routes");
+
 // Set up a static folder (public) for our web app
 app.use(express.static("public"));
 
-var db = require("./models");
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
 
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
 
-mongoose.Promise = Promise;
-mongoose.connect("mongodb://localhost/mongoHeadlines");
+// Use bodyParser in our app
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
-  console.log("Database Error:", error);
-});
+// Designate our public folder as a static directory
+app.use(express.static("public"));
 
-// Main route (simple Hello World Message)
-app.get("/", function(req, res) {
-  res.send("Hello world");
-});
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// Retrieve data from the db
-app.get("/all", function(req, res) {
-  // Find all results from the scrapedData collection in the db
-  db.scrapedData.find({}, function(error, found) {
-    // Throw any errors to the console
-    if (error) {
-      console.log(error);
-    }
-    // If there are no errors, send the data to the browser as json
-    else {
-      res.json(found);
-    }
-  });
-});
+// Have every request go through our route middleware
+app.use(routes);
 
-  // Scrape data from one site and place it into the mongodb db
-  app.get("/scrape", function(req, res) {
-    // Make a request for the news section of `ycombinator`
-    request("https://www.nytimes.com/", function(error, response, html) {
-      // Load the html body from request into cheerio
-      var $ = cheerio.load(html);
-      // For each element with a "title" class
-      $(".title").each(function(i, element) {
-        // Save the text and href of each link enclosed in the current element
-        var title = $(element).children("a").text();
-        var link = $(element).children("a").attr("href");
-  
-        // If this found element had both a title and a link
-        if (title && link) {
-          // Insert the data in the scrapedData db
-          db.scrapedData.insert({
-            title: title,
-            link: link
-          },
-          function(err, inserted) {
-            if (err) {
-              // Log the error if one is encountered during the query
-              console.log(err);
-            }
-            else {
-              // Otherwise, log the inserted data
-              console.log(inserted);
-            }
-          });
-        }
-      });
-    });
-  
-    // Send a "Scrape Complete" message to the browser
-    res.send("Scrape Complete");
-  });
-  
-// Start the server
-app.listen(3000, function() {
-  console.log("App running on port 3000!");
-});
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
 
+// Connect to the Mongo DB
+mongoose.connect(MONGODB_URI);
+
+// Listen on the port
 app.listen(PORT, function() {
-  console.log("App running on port " + PORT + "!");
+  console.log("Listening on port: " + PORT);
 });
+
+
+// // Retrieve data from the db
+// app.get("/all", function(req, res) {
+//   // Find all results from the scrapedData collection in the db
+//   db.scrapedData.find({}, function(error, found) {
+//     // Throw any errors to the console
+//     if (error) {
+//       console.log(error);
+//     }
+//     // If there are no errors, send the data to the browser as json
+//     else {
+//       res.json(found);
+//     }
+//   });
+// });
+
+//   // Scrape data from one site and place it into the mongodb db
+//   app.get("/scrape", function(req, res) {
+//     // Make a request for the news section of `ycombinator`
+//     request("https://www.nytimes.com/", function(error, response, html) {
+//       // Load the html body from request into cheerio
+//       var $ = cheerio.load(html);
+//       // For each element with a "title" class
+//       $(".title").each(function(i, element) {
+//         // Save the text and href of each link enclosed in the current element
+//         var title = $(element).children("a").text();
+//         var link = $(element).children("a").attr("href");
+  
+//         // If this found element had both a title and a link
+//         if (title && link) {
+//           // Insert the data in the scrapedData db
+//           db.scrapedData.insert({
+//             title: title,
+//             link: link
+//           },
+//           function(err, inserted) {
+//             if (err) {
+//               // Log the error if one is encountered during the query
+//               console.log(err);
+//             }
+//             else {
+//               // Otherwise, log the inserted data
+//               console.log(inserted);
+//             }
+//           });
+//         }
+//       });
+//     });
+  
+//     // Send a "Scrape Complete" message to the browser
+//     res.send("Scrape Complete");
+//   });
+  
+// // Start the server
+// app.listen(3000, function() {
+//   console.log("App running on port 3000!");
+// });
+
+// app.listen(PORT, function() {
+//   console.log("App running on port " + PORT + "!");
+// });
 
